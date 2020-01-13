e41c34d46043418392b8d198c322ae98b1db79cf
Fadila kangaye <fkangaye@yahoo.com>
2020-01-12 19:47:09 -0600

    add unused import for to go to next module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import {Http} from '@angular/http';
+import {Http, Response} from '@angular/http';
 import 'rxjs/add/operator/map';
-// import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,7 +9,7 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  getAlbum(id: number){
+  getAlbum(id: number) {
     return this._http.get(this._albumUrl).map((response) => response.json())
   }
 
